b8effca5496a3d74c221e06ed4a8506b526daa56
szchan <mail.szchan@gmail.com>
2026-01-02 11:13:21 +0800

    Add nix-command and flake support

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo nix run 'github:nix-community/disko#disko-install' -- \
+sudo nix --experimental-features "nix-command flakes" \
+  run 'github:nix-community/disko#disko-install' -- \
   --flake 'github:szchan/nixos-config#szchanNixOSStation' \
   --disk main /dev/vda
-
