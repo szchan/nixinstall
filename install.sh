1edaa4cc9b6fec67db488a2a4bea2e876b9d26c0
szchan <mail.szchan@gmail.com>
2026-01-02 11:07:55 +0800

    Fix syntax

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-sudo nix --extra-experimental-features "nix-command flakes" \
-  run github:nix-community/disko#disko-install -- \
-  --flake github:szchan/nixos-config#szchanNixOSStation \
-  --disk main /dev/sda   
-  --write-efi-boot-entries 
+sudo nix run 'github:nix-community/disko#disko-install' -- \
+  --flake 'github:szchan/nixos-config#szchanNixOSStation' \
+  --disk main /dev/vda
+
